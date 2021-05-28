68677bca5c9bb11a2e3d0dfd0505b9e0afcd0c8f
ferjaviperez <78280871+ferjaviperez@users.noreply.github.com>
2021-05-28 17:09:42 -0500

    Initial commit

---
+++ S7ImplemCamara/DataBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace S7ImplemCamara
+{
+    public interface  DataBase
+    {
+        
+        SQLiteAsyncConnection GetConnection();
+
+    }
+}
